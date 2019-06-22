2371efb219bc2da6494ab19ae8bea3e23feeebba
Manny <egalli64@gmail.com>
2019-06-22 22:43:24 +0200

    slide 17

---
+++ src/s17/ErrorGenerator.java
@@ -0,0 +1,25 @@
+package s17;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/s17/error")
+public class ErrorGenerator extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        throw new IllegalStateException("Something awful happened");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
