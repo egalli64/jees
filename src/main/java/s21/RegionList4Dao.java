8056de5bbe70a1dbda9ea63f3964e95ca1d0c171
Manny <egalli64@gmail.com>
2019-12-21 14:33:42 +0100

    Patch for Resource annotation robustness

--- src/main/java/s21/RegionList4Dao.java
+++ src/main/java/s21/RegionList4Dao.java
@@ -2,24 +2,16 @@ package s21;
 
 import java.io.IOException;
 
+import javax.annotation.Resource;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-//import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-
-import javax.annotation.Resource;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.sql.DataSource;
 
 @WebServlet("/s21/regions2")
 public class RegionList4Dao extends HttpServlet {
@@ -31,16 +23,16 @@ public class RegionList4Dao extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        if(ds == null) {
+
+        if (ds == null) {
             try {
-                Context ctx = (Context)new InitialContext().lookup("jdbc/me");
-            } catch (NamingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                Context envContext = (Context) (new InitialContext().lookup("java:/comp/env"));
+                ds = (DataSource) envContext.lookup("jdbc/me");
+            } catch (NamingException ne) {
+                throw new IllegalStateException("Can't resolve JDBC resource", ne);
             }
         }
-        
+
         DaoRegion dao = new DaoRegion(ds);
         request.setAttribute("regions", dao.getAll());
         request.getRequestDispatcher("/s21/regions2.jsp").forward(request, response);
