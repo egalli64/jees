26ddc298389c5a2fb6d09b04d03a0d92a8529fb4
Manny <egalli64@gmail.com>
2020-03-03 15:46:56 +0100

    Class.forName() example added as comment

--- src/main/java/s22/MyContextListener.java
+++ src/main/java/s22/MyContextListener.java
@@ -10,13 +10,6 @@ import javax.servlet.annotation.WebListener;
 public class MyContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         sce.getServletContext().setAttribute("start", LocalTime.now());
-        
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//        } catch (ClassNotFoundException cnfe) {
-//            cnfe.printStackTrace();
-//            throw new IllegalStateException("Can't load JDBC driver " + cnfe.getMessage());
-//        }
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
