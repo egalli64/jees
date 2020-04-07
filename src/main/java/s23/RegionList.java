b35c56969251aeb686b7469611d83de297580f7d
Manny <egalli64@gmail.com>
2020-04-07 18:08:26 +0200

    Resource patch removed

--- src/main/java/s23/RegionList.java
+++ src/main/java/s23/RegionList.java
@@ -3,9 +3,6 @@ package s23;
 import java.io.IOException;
 
 import javax.annotation.Resource;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,22 +20,6 @@ public class RegionList extends HttpServlet {
 
     @Resource(name = "jdbc/me")
     private DataSource ds;
-
-    @Override
-    public void init() throws ServletException {
-        super.init();
-
-        // resource injection not working in Tomcat latest versions (?!)
-        if (ds == null) {
-            logger.info("Resource-not-injected Tomcat patch");
-            try {
-                Context envContext = (Context) (new InitialContext().lookup("java:/comp/env"));
-                ds = (DataSource) envContext.lookup("jdbc/me");
-            } catch (NamingException ne) {
-                throw new ServletException("Can't resolve JDBC resource", ne);
-            }
-        }
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
