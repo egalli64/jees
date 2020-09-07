1fd439c1b767bf2980e72c0aed11522ce0a91f21
Manny <egalli64@gmail.com>
2020-09-07 18:02:02 +0200

    post command

--- src/main/java/s15/Login.java
+++ src/main/java/s15/Login.java
@@ -8,23 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Login
- */
 @WebServlet("/s15/login")
 public class Login extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         String user = request.getParameter("user");
         String password = request.getParameter("password");
-        
+
         request.setAttribute("user", user);
 
         if (user == null || password == null || user.isBlank() || password.isBlank()) {
@@ -33,15 +26,4 @@ public class Login extends HttpServlet {
             request.getRequestDispatcher("/s15/logged.jsp").forward(request, response);
         }
     }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
 }
