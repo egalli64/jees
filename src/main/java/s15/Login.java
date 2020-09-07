086abbd21f1c9bc8cd12c054cf824b8fe8fe3d0a
Manny <egalli64@gmail.com>
2020-09-07 17:02:22 +0200

    minor fix

--- src/main/java/s15/Login.java
+++ src/main/java/s15/Login.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Login
  */
-@WebServlet(name = "/s15/login")
+@WebServlet("/s15/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
