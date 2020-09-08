54cea16de4a942305d48f1a7126a666e4298dff5
Manny <egalli64@gmail.com>
2020-09-08 13:44:25 +0200

    simple solution

--- src/main/java/s21/Choice.java
+++ src/main/java/s21/Choice.java
@@ -8,40 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Choice
- */
 @WebServlet("/s21/choice")
 public class Choice extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Choice() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String[] colors = request.getParameterValues("colors");
+//        String[] colors = request.getParameterValues("colors");
+//        request.setAttribute("values", colors);
 
+        request.getRequestDispatcher("/s21/feedback.jsp").forward(request, response);
     }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
-
 }
--- src/main/webapp/s21/feedback.jsp
+++ src/main/webapp/s21/feedback.jsp
@@ -1,13 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Colors</title>
 </head>
 <body>
     <ul>
-        <li>_color_</li>
+        <c:forEach var="color" items="${paramValues.colors}">
+            <li>${color}</li>
+        </c:forEach>
     </ul>
 </body>
 </html>
