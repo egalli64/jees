775b52342d56b96bdc88dbecf80624366b04e3a2
Manny <egalli64@gmail.com>
2020-10-05 08:43:41 +0200

    minor cleanup

--- src/main/java/s12/Router.java
+++ src/main/java/s12/Router.java
@@ -2,7 +2,6 @@ package s12;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
