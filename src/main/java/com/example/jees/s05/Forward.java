bda417dd5b1f2d04f625450d3939f49e26427f22
Emanuele <egalli64@gmail.com>
2022-03-25 22:17:06 +0100

    forward vs redirect

--- src/main/java/com/example/jees/s05/Forward.java
+++ src/main/java/com/example/jees/s05/Forward.java
@@ -13,33 +13,28 @@ import org.apache.logging.log4j.Logger;
 
 @SuppressWarnings("serial")
 @WebServlet("/s05/send")
-public class Router extends HttpServlet {
-    private static final Logger log = LogManager.getLogger(Router.class);
+public class Forward extends HttpServlet {
+    private static final Logger log = LogManager.getLogger(Forward.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String to = request.getParameter("to");
         log.debug("called for " + to);
 
-        String destination;
-        switch (to) {
-        case "home":
-            log.trace("forward back home");
-            destination = "/";
-            break;
-        case "timer":
+        String destination = switch (to) {
+        case "timer" -> {
             log.trace("forward to timer servlet");
-            destination = "/s02/timer";
-            break;
-        case "checker":
-            log.trace("forward to checker servlet");
-            destination = "/s03/checker";
-            break;
-        default:
-            log.trace("redirect to Apache Tomcat");
-            response.sendRedirect("https://tomcat.apache.org/");
-            return;
+            yield "/s02/timer";
         }
+        case "checker" -> {
+            log.trace("forward to checker servlet");
+            yield "/s03/checker";
+        }
+        default -> {
+            log.trace("forward back home");
+            yield "/";
+        }
+        };
 
         request.getRequestDispatcher(destination).forward(request, response);
     }
---
+++ src/main/java/com/example/jees/s05/Redirect.java
@@ -0,0 +1,24 @@
+package com.example.jees.s05;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@SuppressWarnings("serial")
+@WebServlet("/s05/redirect")
+public class Redirect extends HttpServlet {
+    private static final Logger log = LogManager.getLogger(Redirect.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        log.trace("redirect to Apache Tomcat");
+        response.sendRedirect("https://tomcat.apache.org/");
+    }
+}
