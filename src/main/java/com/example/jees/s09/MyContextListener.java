e05ba4474c413d05dfb1f23c6dd400385c0cc859
Emanuele <egalli64@gmail.com>
2023-02-05 15:29:56 +0100

    more comments

--- src/main/java/com/example/jees/s09/MyContextListener.java
+++ src/main/java/com/example/jees/s09/MyContextListener.java
@@ -7,12 +7,13 @@ package com.example.jees.s09;
 
 import java.time.LocalTime;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 /**
  * Listen on web app events, add an attribute to the servlet context at startup
@@ -24,7 +25,29 @@ public class MyContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         log.traceEntry();
-        sce.getServletContext().setAttribute("start", LocalTime.now());
+
+        // some setup work on the servlet context
+        ServletContext sc = sce.getServletContext();
+
+        // add a custom attribute to the servlet context
+        sc.setAttribute("start", LocalTime.now());
+
+        // Attributes, accessible by enumeration
+//        for (var e = sc.getAttributeNames(); e.hasMoreElements();) {
+//            String name = e.nextElement();
+//            System.out.print(name);
+//            System.out.print(name.equals("start") ? String.format("(%s)", sc.getAttribute(name)) : " ");
+//        }
+
+        // Initialization parameter (see web.xml), accessible by enumeration
+//        for (var e = sc.getInitParameterNames(); e.hasMoreElements();) {
+//             uncomment next lines to access them
+//            String name = e.nextElement();
+//            System.out.printf("%s %s%n", name, sc.getInitParameter(name));
+//        }
+
+        // uncomment the next line to set the web app default session timeout programmatically
+//         sc.setSessionTimeout(1);
     }
 
     @Override
