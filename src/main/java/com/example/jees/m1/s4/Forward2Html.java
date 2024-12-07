b81d2a570f97dcdd8a067bd2558da366ca15bbcc
Emanuele <egalli64@gmail.com>
2024-12-07 23:11:35 +0100

    clarify

---
+++ src/main/java/com/example/jees/m1/s4/Forward2Html.java
@@ -0,0 +1,39 @@
+/*
+ * Introduction to Jakarta Enterprise Edition
+ * 
+ * https://github.com/egalli64/jees
+ */
+package com.example.jees.m1.s4;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * A servlet forwarding to a HTML
+ */
+@SuppressWarnings("serial")
+@WebServlet("/m1/s4/forward2html")
+public class Forward2Html extends HttpServlet {
+    private static final Logger log = LogManager.getLogger(Forward2Html.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        final String destination = "hello.html";
+
+        log.traceEntry(destination);
+
+        request.getRequestDispatcher("hello.html").forward(request, response);
+
+        // split in two steps, for improved readability
+//        var dispatcher = request.getRequestDispatcher(destination);
+//        dispatcher.forward(request, response);
+    }
+}
